b45618bceec237256f6f03421afb43d34f8c8da3
pogodynka202 <uksik-2005@wp.pl>
2025-04-13 21:31:32 +0200

    wip: simple data structure prepared upgraded

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
+from utils.model import users
+from utils.controller import get_user_info
 
-from utils,model import users
-from utils,controller import get_user_info
 
 def main():
     get_user_info(users)
 
 
-    if__name =="__main__":
+if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
---
+++ notatnik.py
@@ -0,0 +1,5 @@
+def tomek(name:str)->None:
+    print(f"cześć {name}, jestem Tomek")
+
+user_name="Maciej"
+tomek(user_name)
